63b1108d15898bf59092e94450a0bda9b2e1e990
Artur P.C. Amaral <artur.portuguez@gmail.com>
2026-02-03 19:10:05 -0300

    Ajuste do template exibir_evento com condicionais e melhoria de estrutura

---
+++ agenda/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from agenda.models import eventos
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Olá MUNDO!!!')
+
+
+def exibir_evento(request):
+    evento = eventos[0]
+    # template = loader.get_template('agenda/exibir_evento.html')
+    # rendered_templated = template.render(context={'evento': evento}, request=request)
+    # return HttpResponse(rendered_templated)
+    return render(request=request, context={'evento': evento}, template_name='agenda/exibir_evento.html')
